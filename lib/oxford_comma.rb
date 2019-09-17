619c18bebebbf226773a41510b58c310fcd87755
Olivia <ortufte@gmail.com>
2019-09-17 20:41:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    return array[0]
+  elsif array.length >= 2 
+    array.join(" and ")
+  else 
+    string = array.join(" , ")
+    string.insert(-14, "and")
+  end
 end
